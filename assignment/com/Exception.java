3d9113e5cd4f217617c8a982db3e85f58e9413bc
NandhiniMB <nandhinibmna@gmail.com>
2021-01-21 22:43:12 +0530

    Modified Throw

--- assignment/com/Exception.java
+++ assignment/com/Exception.java
@@ -1,19 +1,35 @@
 package assignment.com;
 
+import java.util.Scanner;
+
 public class Exception {
 
+	
+	void ExampleMethod1(int age) throws ArithmeticException{
+		
+		
+		 if(age<12 && age<40) {
+	         throw new ArithmeticException("Student is not eligible for registration"); 
+	      }
+		 
+	}
 	public static void main(String[] args) {
 		
-		try {
-			int c= 10/0;
-
-		}
-		catch (ArithmeticException e) {
-			System.out.println(e);
-			}
-		
-		finally {
-			System.out.println("In final");
-		}
+//		try {
+//			int c= 10/0;
+//
+//		}
+//		catch (ArithmeticException e) {
+//			System.out.println(e);
+//			}
+//		
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter age of person");
+		int age=in.nextInt();
+		Exception e = new Exception();
+		e.ExampleMethod1(age);
+//		finally {
+//			System.out.println("In final");
+//		}
 	}
 }
